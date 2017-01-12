03c8349043b5ebe4b25abaa2b64b023f1b81feaf
jingaiweiyi <lonelyboy9527@sina.com>
2017-01-12 20:00:48 +0800

    Logging

--- src/cc/openhome/MyClass1.java
+++ src/cc/openhome/MyClass1.java
@@ -1,7 +1,15 @@
 package cc.openhome;
 
 public class MyClass1 {
+	// 日志API简介
 	public static void exp1() {
-		System.out.println("exp1");
+		System.out.println("exp1 -> 日志API简介");
+		
+		/* java.util.logging包提供了日志功能相关类与接口，
+		 * 
+		 * 它们是从JDK1.4之后加入标准API，不必额外配置日志组件，就可在标准Java平台使用是其好处。
+		 * 
+		 * 使用日志等起点是Logger类
+		 * */
 	}
 }
