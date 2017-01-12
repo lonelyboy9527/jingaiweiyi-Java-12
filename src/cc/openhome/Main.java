b643bbef3d05bdaf148671aa214195a70c424b42
jingaiweiyi <lonelyboy9527@sina.com>
2017-01-12 19:50:31 +0800

    12.通用API

---
+++ src/cc/openhome/Main.java
@@ -0,0 +1,14 @@
+package cc.openhome;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		exp1();
+	}
+	
+	public static void exp1() {
+		MyClass1.exp1();
+	}
+
+}
---
+++ src/cc/openhome/MyClass1.java
@@ -0,0 +1,7 @@
+package cc.openhome;
+
+public class MyClass1 {
+	public static void exp1() {
+		System.out.println("exp1");
+	}
+}
